62137334a0ef83aea9e043a9723886d9ecd0d31f
envizar <silent.gate@ya.ru>
2024-12-24 20:26:10 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<script setup>
+import Card from "@/components/Card.vue";
+import {ref, onMounted, computed} from 'vue';
+import {useDateFormat, useNow} from "@vueuse/core";
+
+const currentTime = ref();
+
+function updateTime() {
+  const now = new Date();
+  const utc3 = new Date(now.getTime() + now.getTimezoneOffset()*60*1000 + 3*60*60*1000);
+  currentTime.value = useDateFormat(utc3, 'HH:mm:ss')
+}
+
+// Update the time every second
+onMounted(() => {
+  updateTime();
+  setInterval(updateTime, 1000);
+});
+
+const age = computed(() => {
+  const now = new Date()
+  const born = new Date(1182888000 * 1000)
+  return now.getFullYear() - born.getFullYear()
+})
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 items-center justify-center mb-8 mt-16 lg:mt-0">
+    <img src="/avatar.jpg" alt="avatar" class="w-20 h-20 rounded-lg avatar">
+    <span class="text-2xl font-700 color-white font-['JetBrains_Mono',_serif]">envizar</span>
+  </div>
+  <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 max-w-120 lg:w-200 lg:max-w-200">
+    <Card icon="lucide:scroll-text" title="Основное">
+      <ul>
+        <li>Возраст: {{ age }} лет</li>
+        <li>Пол: Мужской</li>
+        <li>Страна: Россия</li>
+        <li>Время: {{ currentTime }} (UTC+3)</li>
+      </ul>
+    </Card>
+
+    <Card icon="lucide:link" title="Ссылки">
+      <ul>
+        <li>Telegram:
+          <a href="https://t.me/envizar" target="_blank">Аккаунт</a> &bull;
+          <a href="https://t.me/meza_inc">Канал</a>
+        </li>
+        <li>GitHub:
+          <a href="https://github.com/envizar">Профиль</a> &bull;
+          <a href="https://github.com/mezamc">Организация</a>
+        </li>
+        <li>Проект:
+          <a href="https://book.meza.one">MezaBook</a>
+        </li>
+        <li>YouTube:
+          <a href="https://www.youtube.com/@envizar">Личный канал</a>
+        </li>
+      </ul>
+    </Card>
+
+    <Card icon="lucide:box" title="Minecraft">
+      <p>Более трёх лет создаю Minecraft сервера.
+      Настраиваю и пишу плагины, делаю оформление,
+      даже немного консультирую людей по этим вопросам.</p>
+      <p>Самый крупный проект - ныне закрытый
+      мистический сервер SiimpAlone.</p>
+    </Card>
+
+    <Card icon="lucide:list-checks" title="Скиллы">
+      <ul>
+        <li>Пишу Minecraft плагины на Kotlin</li>
+        <li>Создаю сайты на Vue/Nuxt</li>
+        <li>Немного графический дизайнер (использую Adobe Illustrator)</li>
+        <li>I use NixOS btw</li>
+      </ul>
+    </Card>
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+.avatar {
+  box-shadow: 0 0 5rem rgba(255, 0, 0, .25);
+  z-index: 1;
+  outline: solid transparent 0px;
+  transition: 150ms all ease-in-out;
+  &:hover {
+    outline: solid #ea4c4c 2px;
+    outline-offset: 4px;
+    box-shadow: 0 0 5rem rgba(255, 0, 0, 0.7);
+    transform: scale(1.05);
+  }
+  &:active {
+    transform: scale(.9);
+    filter: brightness(2);
+    outline-color: #ffc7c7;
+    box-shadow: 0 0 8rem rgba(255, 53, 53, 0.75);
+    outline-offset: 2px;
+  }
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+
+const props = defineProps<{
+  icon?: string
+  title?: string
+}>()
+
+</script>
+
+<template>
+  <div class="flex flex-col bg-black/20 p-6.5 p-t-5 p-b-5 rounded-lg border-solid border-1 border-transparent hover:border-red-2/30 transition gap-2.5">
+    <h2 class="m-0 p-0 flex items-center gap-2.5">
+      <Icon :icon v-if="icon"/> {{ title }}
+    </h2>
+    <div class="color-[#DBD1D1] flex flex-col gap-2.5">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
